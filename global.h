25966c8a868ef70a2e3da53196cdb2d7a17d3864
yf560 <113236894+yf560@users.noreply.github.com>
2025-05-28 10:22:29 +0900

    Update global.h

--- global.h
+++ global.h
@@ -1,38 +1,11 @@
-#ifndef GLOBAL_H
-#define GLOBAL_H
+#ifndef LOG_H
+#define LOG_H
 
-#include <stdbool.h>
-#include <time.h>  // 🔥 시간 계산을 위해 추가
-// 게임상태
-extern int pause_access;
-extern int store_access;
-extern int HEIGHT;
-extern int WIDTH;
-extern time_t start_time;
-extern int winning_streak;
+#include <stdio.h>
 
+// 로그 파일 작성 함수
+void write_log_file(const char *upgrade, const char *logfilename);
+// 로그 파일 읽기 함수 // 최근 5개 + 없음 표시 가능능
+extern void read_log_file(const char *logfilename, int y_offset,int x_offset);
 
-
-
-
-//플레이어 데이터
-extern int data;
-extern int atk_stat;
-extern int dfs_stat;
-
-extern int pve_start_bit;
-extern int pve_data_intake;
-extern int pve_strong_atk_stat;
-
-extern int pvp_charge_minus;
-extern float pvp_counter_atk_power_stat;
-extern float pvp_charge_strong;
-
-extern int buy_atk_cnt;
-extern int buy_dfs_cnt;
-
-extern int ability_sort;
-extern bool ability_dup_check[7];
-
-
-#endif
+#endif // LOG_H
