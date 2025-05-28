d0d0acf6437e940c9c2fb6b62679243c0e156d8a
yf560 <113236894+yf560@users.noreply.github.com>
2025-05-28 10:25:05 +0900

    Update Diver_ui.h

--- Diver_ui.h
+++ Diver_ui.h
@@ -14,6 +14,7 @@ void reset_stat();
 //screen
 void start_screen(const char *client_name); // 시작 화면
 void guide_screen();
+void loading_screen();
 
 void pause_screen(int store_access1);  // 일시 정지 화면
 int pause_choice(int store_access1);
