4803785ad0da61fca53e9f102c6b5d712dd118dc
yf560 <113236894+yf560@users.noreply.github.com>
2025-06-24 12:51:17 +0900

    Update log.h

--- log.h
+++ log.h
@@ -6,6 +6,6 @@
 // 로그 파일 작성 함수
 void write_log_file(const char *upgrade, const char *logfilename);
 // 로그 파일 읽기 함수 // 최근 5개 + 없음 표시 가능 + 닉네임 확인후 조치
-extern void read_log_file(const char *logfilename, int y_offset,int x_offset);
+void read_log_file(const char *logfilename, int y_offset,int x_offset);
 
 #endif 
